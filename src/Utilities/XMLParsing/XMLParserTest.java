e9394d4baac26dcfed17096953dbc8e57de22978
Austin Kyker <amk66@duke.edu>
2014-11-10 17:51:40 -0500

    added xml parsing utility and tested it

---
+++ src/Utilities/XMLParsing/XMLParserTest.java
@@ -0,0 +1,32 @@
+package Utilities.XMLParsing;
+
+import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class XMLParserTest {
+    
+    @Test
+    public void XMLParsingTest() throws FileNotFoundException, ParserConfigurationException, 
+    SAXException, IOException {
+        File testFile = new File("./src/Utilities/XMLParsing/test.xml");
+        XMLParser parser = new XMLParser(testFile);
+        List<String> movementsList = parser.getValuesFromTag("movement");
+        assertEquals(3, movementsList.size());
+        String[] movementVals = new String[]{"FastMovement", "SlowMovement", "NoMovement"};
+        for(int i = 0; i < movementsList.size(); i++){
+            assertEquals(movementVals[i], movementsList.get(i));
+        }           
+        List<String> attackList = parser.getValuesFromTag("attack");
+        assertEquals(2, attackList.size());
+        String[] attackVals = new String[]{"BigAttack", "SmallAttack"};
+        for(int i = 0; i < attackList.size(); i++){
+            assertEquals(attackVals[i], attackList.get(i));
+        } 
+    }
+}
