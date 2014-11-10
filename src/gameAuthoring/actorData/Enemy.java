2b6503925d7d1d7aff0d526a0f559834e60c00ac
Austin Kyker <amk66@duke.edu>
2014-11-09 20:41:40 -0500

    added front end enemy

---
+++ src/gameAuthoring/actorData/Enemy.java
@@ -0,0 +1,10 @@
+package gameAuthoring.actorData;
+
+public class Enemy {
+    public static enum MOVE { FAST, MEDIUM, SLOW};
+    public static enum ATTACK { BIG, SMALL, NONE };
+    
+    private MOVE myMove;
+    private ATTACK myAttack;
+    
+}
