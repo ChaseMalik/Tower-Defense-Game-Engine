d243f417e13cc812ece23f980f25441229530af0
Allan Kiplagat <allan.kiplagat@duke.edu>
2014-11-10 22:43:45 -0500

    added launch class

--- src/mainClasses/Main.java
+++ src/mainClasses/Main.java
@@ -1,5 +1,15 @@
-package gamePlayer;
+package mainClasses;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage stage) throws Exception {
+        
+    }
 }
