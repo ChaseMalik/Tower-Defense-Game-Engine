d098d571fd2dd46c76acd7cc18d0e527f17eed54
Chase Malik <chase.malik@duke.edu>
2014-11-16 00:06:30 -0500

    changed getpoints in frontend

--- src/gameEngine/actors/behaviors/BaseMovementBehavior.java
+++ src/gameEngine/actors/behaviors/BaseMovementBehavior.java
@@ -1,6 +1,8 @@
 package gameEngine.actors.behaviors;
 
+import java.util.List;
 import gameAuthoring.scenes.pathBuilding.pathComponents.routeToPointTranslation.BackendRoute;
+import gameAuthoring.scenes.pathBuilding.pathComponents.routeToPointTranslation.VisibilityPoint;
 
 /**
  * @author $cotty $haw
@@ -8,11 +10,11 @@ import gameAuthoring.scenes.pathBuilding.pathComponents.routeToPointTranslation.
  */
 public abstract class BaseMovementBehavior implements IBehavior {
 
-    protected BackendRoute myRoute;
+    protected List<VisibilityPoint> myRoute;
     protected double mySpeed;
     
     public BaseMovementBehavior(BackendRoute route, double speed){
-        myRoute = route;
+        myRoute = route.getPoints();
         mySpeed = speed;
     }
 }
--- src/gameEngine/actors/behaviors/LinearMovement.java
+++ src/gameEngine/actors/behaviors/LinearMovement.java
@@ -4,15 +4,21 @@ import javafx.scene.Group;
 import gameAuthoring.scenes.pathBuilding.pathComponents.routeToPointTranslation.BackendRoute;
 import gameEngine.actors.BaseActor;
 
-public class LinearMovement extends BaseMovementBehavior {
-
-    public LinearMovement (BackendRoute route, double speed) {
+public class LinearMovementBehavior extends BaseMoveBehavior {
+    int myIndex = 0;
+    public LinearMovementBehavior (BackendRoute route, double speed) {
         super(route, speed);
     }
 
     @Override
     public void execute (BaseActor actor) {
-        
+        if(myIndex == 0){
+            actor.setVisible(true);
+            actor.setX(myRoute.get);
+        }
+        double x = actor.getX();
+        double y = actor.getY();
+        myRoute.
     }
 
 }
