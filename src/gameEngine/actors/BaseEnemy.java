27ecdd3742de13341479344967384233d8d523ed
timeshp <timeshp@gmail.com>
2014-11-23 01:50:20 -0500

    fixed copy

--- src/gameEngine/actors/BaseEnemy.java
+++ src/gameEngine/actors/BaseEnemy.java
@@ -3,17 +3,26 @@ package gameEngine.actors;
 import gameAuthoring.scenes.pathBuilding.pathComponents.routeToPointTranslation.BackendRoute;
 import gameEngine.actors.behaviors.BaseMovementBehavior;
 import gameEngine.actors.behaviors.IBehavior;
+import java.util.List;
 import java.util.Map;
 
 public class BaseEnemy extends RealActor {
-        
     
-    public BaseEnemy (Map<String, IBehavior> behaviors, String image, String name, double range, ProjectileInfo projectile, BackendRoute route) {
+    private List<BackendRoute> myRoutes;
+    
+    public BaseEnemy (Map<String, IBehavior> behaviors, String image, String name, double range, ProjectileInfo projectile, List<BackendRoute> route) {
         super(behaviors, image, name, range,projectile);
-        ((BaseMovementBehavior)behaviors.get("movement")).setRoute(route);
+        myRoutes=route;
+        ((BaseMovementBehavior)behaviors.get("movement")).setRoute((route.get((int)(Math.random()*route.size()))));
     }
-    public BaseEnemy (Map<String, IBehavior> behaviors, String image, String name, double range, BackendRoute route) {
+    public BaseEnemy (Map<String, IBehavior> behaviors, String image, String name, double range, List<BackendRoute> route) {
         super(behaviors, image, name, range);
-        ((BaseMovementBehavior)behaviors.get("movement")).setRoute(route);
+        myRoutes=route;
+        ((BaseMovementBehavior)behaviors.get("movement")).setRoute((route.get((int)(Math.random()*route.size()))));
+    }
+    
+    public BaseActor copy(){
+        Map<String, IBehavior> cBehaviors=copyBehaviors();
+        return new BaseEnemy(cBehaviors, myImagePath, myName, myRange, myProjectile, myRoutes);
     }
 }
