05dfd17e3e5af0c93aa3ce1ae9e414df01c4ef9c
timeshp <timeshp@gmail.com>
2014-11-23 02:00:25 -0500

    fixed error

--- src/gameEngine/actors/behaviors/RangeAttack.java
+++ src/gameEngine/actors/behaviors/RangeAttack.java
@@ -18,7 +18,7 @@ public abstract class RangeAttack extends BaseAttack{
     protected void shootActorFromActor(BaseActor target, BaseActor actor){
         RealActor shooter=(RealActor) actor;
         BackendRoute route=new BackendRoute(new Point2D(shooter.getX(), shooter.getY()), new Point2D(target.getX(),(target.getY()))); 
-        LinearMovement move=new LinearMovement(shooter.getProjectile().getSpeed()); 
+        LinearMovement move=new LinearMovement(shooter.getProjectile().getSpeed(),route); 
         BaseProjectile projectile=new BaseProjectile(move); 
         List<BaseActor> pList=new ArrayList<>();
         pList.add(projectile);
