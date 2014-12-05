fef588eb414d42aab6c61e22ca5fec048606b5f2
thefreshduke <scottyshaw1@gmail.com>
2014-12-05 00:07:09 -0500

    Switched ClientApplet.java layout to BorderPane from JavaFX

--- src/chatroom/ClientApplet.java
+++ src/chatroom/ClientApplet.java
@@ -1,7 +1,10 @@
 package chatroom;
 
 import java.applet.*;
-import java.awt.*;
+import java.awt.LayoutManager;
+
+//import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 
 public class ClientApplet extends Applet {
 
@@ -13,7 +16,9 @@ public class ClientApplet extends Applet {
     public void init() {
         String host = getParameter("host");
         int port = Integer.parseInt(getParameter("port"));
-        setLayout(new BorderLayout());
+//        setLayout(new BorderLayout());
+//        setLayout(new Border());
+        setLayout((LayoutManager) new BorderPane());
         add("Center", new Client(host, port));
     }
 }
