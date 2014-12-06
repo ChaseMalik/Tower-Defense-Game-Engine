85127c543d0d5fbddd183a9ff7517bcf7d36b486
Austin Kyker <amk66@duke.edu>
2014-12-06 17:00:29 -0500

    tower region selection now saved after going to another scene

--- src/gameAuthoring/scenes/pathBuilding/buildingPanes/towerRegions/TowerRegionsPane.java
+++ src/gameAuthoring/scenes/pathBuilding/buildingPanes/towerRegions/TowerRegionsPane.java
@@ -15,18 +15,29 @@ public class TowerRegionsPane extends BuildingPane {
     private GridPane myPane;
     private Path myPath;
     private boolean[][] myBackendGrid;
+    //Parameter representing whether or not the user has
+    //used this pane (has clicked on the tower regions option pane)
+    private boolean isRegionSelected;
 
     public TowerRegionsPane (Group group, Path path) {
         super(group);  
         myPath = path;
         myPane = new GridPane();
+        myPane.setPrefSize(BuildingPane.DRAW_SCREEN_WIDTH, 
+                           AuthorController.SCREEN_HEIGHT);
+        isRegionSelected = false;
     }
     
     @Override
     public void executeEnterFunction() {
-        myPane.getChildren().clear();
-        myPane.setPrefSize(BuildingPane.DRAW_SCREEN_WIDTH, 
-                           AuthorController.SCREEN_HEIGHT);
+        if(!isRegionSelected) {
+            suggestNoTowerPlacementOnPath();
+            isRegionSelected = true;
+        }
+        myGroup.getChildren().add(myPane);
+    }
+
+    private void suggestNoTowerPlacementOnPath () {
         for(int row = 0; row < NUM_ROWS; row++){
             for(int col = 0; col < NUM_COLS; col++){
                 Tile tile = new Tile(row, col);
@@ -36,7 +47,6 @@ public class TowerRegionsPane extends BuildingPane {
                 }
             }
         }
-        myGroup.getChildren().add(myPane);
     }
     
     @Override
