acc743ee9e2e29336858c2d788e40a5e4b6d99d9
Allan Kiplagat <allan.kiplagat@duke.edu>
2014-12-06 22:13:09 -0500

    welcome screen partially done

---
+++ src/gamePlayer/mainClasses/welcomeScreen/options/MultiPlayerOptions.java
@@ -0,0 +1,28 @@
+package gamePlayer.mainClasses.welcomeScreen.options;
+
+import gamePlayer.mainClasses.guiBuilder.GuiConstants;
+import gamePlayer.mainClasses.guiBuilder.GuiText;
+import gamePlayer.mainClasses.welcomeScreen.Options;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+public class MultiPlayerOptions extends Options {
+    private Button newGameOption;
+    private Button joinGameOption;
+
+    public MultiPlayerOptions() {
+        newGameOption = createOptionButton(GuiConstants.TEXT_GEN.get(GuiText.NEW_GAME));
+        joinGameOption = createOptionButton(GuiConstants.TEXT_GEN.get(GuiText.JOIN_GAME));
+
+        this.getChildren().add(newGameOption);
+        this.getChildren().add(joinGameOption);
+    }
+
+    public Node getNewGameOption () {
+        return newGameOption;
+    }
+
+    public Node getJoinGameOption () {
+        return joinGameOption;
+    }
+}
