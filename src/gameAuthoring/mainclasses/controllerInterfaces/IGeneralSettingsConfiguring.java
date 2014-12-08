9a86cad4e6a8659fe1aafe25ab1c8857983ddf42
Austin Kyker <amk66@duke.edu>
2014-12-08 05:59:49 -0500

    author controller refactoring

--- src/gameAuthoring/mainclasses/controllerInterfaces/IGeneralSettingsConfiguring.java
+++ src/gameAuthoring/mainclasses/controllerInterfaces/IGeneralSettingsConfiguring.java
@@ -2,20 +2,22 @@ package gameAuthoring.mainclasses.controllerInterfaces;
 
 import utilities.GSON.objectWrappers.GeneralSettingsWrapper;
 
+
 /**
  * An interface for selecting general settings for the game (player's starting
  * gold, health, background sound, etc).
+ * 
  * @author Austin Kyker
  *
  */
 public interface IGeneralSettingsConfiguring {
-	void setGeneralSettings(GeneralSettingsWrapper generalSettingsWrapper);
-	
-	/**
-	 * 
-	 * @param gameName
-	 * @param singleType: SinglePlayer or Co-Op
-	 */
+    void setGeneralSettings (GeneralSettingsWrapper generalSettingsWrapper);
+
+    /**
+     * 
+     * @param gameName
+     * @param singleType: SinglePlayer or Co-Op
+     */
     void makeDirectory (String gameName, String gameType);
 
 }
