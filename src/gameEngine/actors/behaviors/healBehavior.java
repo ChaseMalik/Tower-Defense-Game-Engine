57b6b6fa556244eea3951e2665e5d63e2dd80aa2
timeshp <timeshp@gmail.com>
2014-12-08 07:04:28 -0500

    added heal behavior got rid of println

---
+++ src/gameEngine/actors/behaviors/healBehavior.java
@@ -0,0 +1,38 @@
+package gameEngine.actors.behaviors;
+
+import java.util.List;
+import gameEngine.healthUpdate;
+import gameEngine.actors.BaseActor;
+
+public class healBehavior extends BaseOnHitBehavior{
+    
+    private double myHeal;
+    public healBehavior(List<Double> list){
+        super(list);
+        myHeal=list.get(1);
+    }
+    @Override
+    public IBehavior copy () {
+        // TODO Auto-generated method stub
+        return new healBehavior(myList);
+    }
+
+    @Override
+    public void during (BaseActor actor) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void start (BaseActor actor) {
+        // TODO Auto-generated method stub
+        actor.changeAndNotify(new healthUpdate(myHeal));
+    }
+
+    @Override
+    public void end (BaseActor actor) {
+        // TODO Auto-generated method stub
+        myDuration=myInitialDuration;
+    }
+
+}
