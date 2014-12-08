047e81b0f385c2e41711fcb6e2f3a4abdad65c91
Chase Malik <chase.malik@duke.edu>
2014-12-08 06:07:24 -0500

    Readded game + remove print statements:

--- src/gameEngine/SingleThreadedEngineManager.java
+++ src/gameEngine/SingleThreadedEngineManager.java
@@ -262,7 +262,6 @@ public class SingleThreadedEngineManager implements Observer, UpdateInterface,
 	}
 
 	private void gameUpdate() {
-		System.out.println("gameupdate");
 		addEnemies();
 		updateActors(myTowerGroup);
 		updateActors(myEnemyGroup);
@@ -288,7 +287,6 @@ public class SingleThreadedEngineManager implements Observer, UpdateInterface,
 	private void addEnemies() {
 
 		if (duration <= 0) {
-			System.out.println("add enemies");
 			duration += myIntervalBetweenEnemies;
 			BaseEnemy enemy = myEnemiesToAdd.poll();
 			if (enemy == null)
@@ -369,7 +367,6 @@ public class SingleThreadedEngineManager implements Observer, UpdateInterface,
 	public void resume() {
 		myTimeline.play();
 		myPausedFlag = false;
-		System.out.println("resume ");
 	}
 
 	public Collection<TowerInfoObject> getAllTowerTypeInformation() {
