fe6ee3fd1218061063af68883ae3d214a281a25f
Austin Kyker <amk66@duke.edu>
2014-12-15 00:28:54 -0500

    analysis masterpiece

--- src/utilities/JavaFXutilities/DragAndDropFilePanes/imagePanes/DragAndDropCopyImagePane.java
+++ src/utilities/JavaFXutilities/DragAndDropFilePanes/imagePanes/DragAndDropCopyImagePane.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Austin Kyker
+
 package utilities.JavaFXutilities.DragAndDropFilePanes.imagePanes;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
@@ -7,18 +10,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import utilities.errorPopup.ErrorPopup;
 
+
 public class DragAndDropCopyImagePane extends DragAndDropImagePane {
 
     private String myFileDestination;
 
-    public DragAndDropCopyImagePane (double width, double height, String fileDestination) {
+    public DragAndDropCopyImagePane (double width, double height, 
+                                     String fileDestination) {
         super(width, height);
         myFileDestination = fileDestination;
     }
 
     @Override
     protected void actOnFile (File file) {
-        File targetFile = new File(myFileDestination + file.getName().toString());  
+        File targetFile = new File(myFileDestination + file.getName().toString());
         myFile = targetFile;
         try {
             Files.copy(file.toPath(), targetFile.toPath(), REPLACE_EXISTING);
@@ -26,6 +31,6 @@ public class DragAndDropCopyImagePane extends DragAndDropImagePane {
         }
         catch (IOException e) {
             new ErrorPopup(Constants.IMG_FILE_NOT_FOUND);
-        }       
+        }
     }
 }
--- src/utilities/JavaFXutilities/DragAndDropFilePanes/imagePanes/DragAndDropFilePane.java
+++ src/utilities/JavaFXutilities/DragAndDropFilePanes/imagePanes/DragAndDropFilePane.java
@@ -1,7 +1,12 @@
+// This entire file is part of my masterpiece.
+// Austin Kyker
+
 package utilities.JavaFXutilities.DragAndDropFilePanes.imagePanes;
 
+import gameAuthoring.mainclasses.Constants;
 import java.io.File;
 import java.util.Observable;
+import utilities.multilanguage.MultiLanguageUtility;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.input.DragEvent;
@@ -10,16 +15,18 @@ import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+
 /**
  * This utility allows for the creation of drag and drop file panes.
  * Simply specify the extensions of files you accept, and where you want
  * the file to be dropped. The class extends Observable so you can
  * set up an observer class to listen for when a file is dropped.
+ * 
  * @author Austin Kyker
  *
  */
 public abstract class DragAndDropFilePane extends Observable {
-    
+
     private static final int LAYOUTX_ADJ = 50;
     private static final int LAYOUTY_ADJ = 20;
 
@@ -32,22 +39,24 @@ public abstract class DragAndDropFilePane extends Observable {
      * @param validExtensions The acceptable extensions drag and dropped file. Ex: [.jpg, .png]
      * @param fileDestination where you want the file dragged and dropped to be saved.
      */
-    public DragAndDropFilePane(double width, double height, String[] validExtensions) {
+    public DragAndDropFilePane (double width, double height, String[] validExtensions) {
         myValidExtensions = validExtensions;
         myContainer = new VBox();
         myContainer.setAlignment(Pos.CENTER);
         myDragAndDropPane = new VBox();
         myDragAndDropPane.setPrefSize(width, height);
-        myDragAndDropPane.setOnDragOver(event->handleDragOver(event));
-        myDragAndDropPane.setOnDragExited(event->handleDragExit(event));
-        myDragAndDropPane.setOnDragDropped(event->handleFileDrop(event));
+        myDragAndDropPane.setOnDragOver(event -> handleDragOver(event));
+        myDragAndDropPane.setOnDragExited(event -> handleDragExit(event));
+        myDragAndDropPane.setOnDragDropped(event -> handleFileDrop(event));
         showInstructionLabel(width, height);
         myContainer.getChildren().add(myDragAndDropPane);
     }
 
     private void showInstructionLabel (double width, double height) {
         Pane labelPane = new Pane();
-        Label label = new Label("Drag and Drop");
+        Label label = new Label();
+        label.textProperty().bind(MultiLanguageUtility.getInstance()
+                                          .getStringProperty(Constants.DRAG_AND_DROP));
         label.setLayoutX(width / 2 - LAYOUTX_ADJ);
         label.setLayoutY(height / 2 - LAYOUTY_ADJ);
         labelPane.getChildren().add(label);
@@ -61,7 +70,7 @@ public abstract class DragAndDropFilePane extends Observable {
             success = true;
             for (File file : db.getFiles()) {
                 String fileName = file.getPath();
-                for (String extension:myValidExtensions) {
+                for (String extension : myValidExtensions) {
                     if (fileName.toLowerCase().contains(extension)) {
                         actOnFile(file);
                         break;
@@ -73,6 +82,11 @@ public abstract class DragAndDropFilePane extends Observable {
         event.consume();
     }
 
+    /**
+     * Subclasses will be able to implement a method that is called when
+     * the user drags and drops a file (e.g. copy file to specified directory).
+     * @param file
+     */
     protected abstract void actOnFile (File file);
 
     private void handleDragExit (DragEvent event) {
